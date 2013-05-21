2fcf125c0d1c87060abdef5a16e9dc73e92d8342
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-21 00:31:20 -0400

    some functionality tested. not yet ready though

--- tests/test_learn.m
+++ tests/test_learn.m
@@ -1,2 +1,54 @@
-function test_learn()
+% function test_learn()
 % test learn++
+clc;
+clear;
+close all;
+K = 5;
+
+addpath('../src/');
+load ionosphere
+u = unique(Y);
+labels = zeros(numel(Y), 1);
+
+for n = 1:numel(Y)
+  for c = 1:numel(u)
+    if u{c} == Y{n}
+      labels(n) = c;
+      break
+    end
+  end
+end
+
+i = randperm(numel(Y));
+data = X(i, : );
+labels = labels(i);
+clear Description X Y c i n u
+
+cv = cvpartition(numel(labels),'k',K);
+z = zeros(numel(labels),1);
+for k = 1:K-1
+  z = z + (training(cv,k)>0);
+end
+ts_idx = find(z == K - 1);
+tr_idx = find(z ~= K - 1);
+
+
+data_tr = data(tr_idx, :);
+data_te = data(ts_idx, :);
+labels_tr = labels(tr_idx);
+labels_te = labels(ts_idx);
+
+cv = cvpartition(numel(labels_tr),'k',K);
+for k = 1:K
+  data_tr_cell{k} = data_tr(training(cv,k)==0, :);
+  labels_tr_cell{k} = labels_tr(training(cv,k)==0);
+end
+clear K cv data labels z tr_idx ts_idx k data_tr labels_tr 
+
+model.type = 'CART';
+net.base_classifier = model;
+net.iterations = 3;
+net.mclass = numel(unique(labels_te));
+
+[net,errs] = learn(net, data_tr_cell, labels_tr_cell, data_te, labels_te);
+plot(errs)
