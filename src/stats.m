1eee2a74c5132342251288122647e8aadb8af2d0
Gregory Ditzler <gregory.ditzler@gmail.com>
2013-05-21 12:54:52 -0400

    added functionality to stats.m for recall, fmeasre, gmean, error, and precision calculations. added gnu header as well.

--- src/stats.m
+++ src/stats.m
@@ -9,6 +9,23 @@ function [f_measure,g_mean,recall,precision,err] = stats(f, h, mclass)
 %     @precision
 %     @err
 %
+
+%     stats.m
+%     Copyright (C) 2013 Gregory Ditzler
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 F = index2vector(f, mclass);
 H = index2vector(h, mclass);
 
